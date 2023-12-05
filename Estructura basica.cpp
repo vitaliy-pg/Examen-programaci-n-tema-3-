54ed3acd259a26017dac4e09d278de397281c591
vitaliy-pg <aljndrvt@myuax.com>
2023-12-05 10:53:42 +0100

    creo una funcion para obtener el valor de la variable

--- Estructura basica.cpp
+++ Estructura basica.cpp
@@ -14,4 +14,5 @@ public :
     void setVariable(const std::string& name, const Variant& value) {
         symbolTable[name] = value;
     }
-
+    Variant getVariable(const std::string& name, const Variant& defaultValue = Variant()) const {
+        auto it = symbolTable.find(name);
